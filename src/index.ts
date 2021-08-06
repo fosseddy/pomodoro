b89b7059678de87228538bc58d4b73b127a81310
Mark Fosseddy <markfosseddy@gmail.com>
2021-08-07 04:02:31 +0500

    add pomodoro timer logic

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,8 @@ type Timer = {
 type Pomodoro = {
   cycle: Cycle;
   timer: Timer;
+  interval: number;
+  sessionCount: number;
 }
 
 function printTime(s: Seconds): string {
@@ -35,6 +37,64 @@ function printTime(s: Seconds): string {
   const sec = s % 60;
 
   return [min, sec].map(v => String(v).padStart(2, "0")).join(":");
+}
+
+function resetInterval(p: Pomodoro) {
+  if (p.interval != -1) {
+    clearInterval(p.interval);
+    p.interval = -1;
+  }
+}
+
+function startTimer(p: Pomodoro) {
+  console.log("Timer Started");
+  p.timer.state = TimerState.Ticking;
+
+  if (p.cycle.name === CycleName.Work) {
+    p.sessionCount += 1;
+  }
+
+  p.interval = window.setInterval(() => {
+    if (p.timer.value === 0) {
+      finishTimer(p);
+    } else {
+      p.timer.value -= 1;
+    }
+  }, 1000);
+}
+
+function finishTimer(p: Pomodoro) {
+  console.log("Timer Finished");
+  p.timer.state = TimerState.Finished;
+  resetInterval(p);
+}
+
+function pauseTimer(p: Pomodoro) {
+  console.log("Timer Paused");
+  p.timer.state = TimerState.Paused;
+  resetInterval(p);
+}
+
+function nextTimer(p: Pomodoro) {
+  console.log("Next Timer");
+  p.timer.state = TimerState.Idle;
+
+  if (p.cycle.name === CycleName.Break || p.cycle.name === CycleName.LongBreak) {
+    if (p.cycle.name === CycleName.LongBreak) {
+      p.sessionCount = 0;
+    }
+    p.cycle.name = CycleName.Work;
+    p.cycle.time = 5;
+    p.timer.value = 5;
+  } else if (p.sessionCount >= 3) {
+    p.cycle.name = CycleName.LongBreak;
+    p.cycle.time = 3;
+    p.timer.value = 3;
+  } else {
+    p.cycle.name = CycleName.Break;
+    p.cycle.time = 1;
+    p.timer.value = 1;
+  }
 }
 
 function main() {
@@ -46,21 +106,12 @@ function main() {
     timer: {
       value: 5,
       state: TimerState.Idle
-    }
+    },
+    interval: -1,
+    sessionCount: 0
   };
 
-  console.log("Timer Started");
-  p.timer.state = TimerState.Ticking;
-
-  let id = setInterval(() => {
-    if (p.timer.value === 0) {
-      console.log("Timer Finished");
-      p.timer.state = TimerState.Finished;
-      clearInterval(id);
-    } else {
-      p.timer.value -= 1;
-    }
-  }, 1000);
+  console.log(p);
 }
 
 main();
