9f04bd24ebea1e0379a64ee65baa91ae4d06d939
Mark Fosseddy <markfosseddy@gmail.com>
2021-08-09 02:37:11 +0500

    project clean up

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,128 @@
 import React from "react";
+import { usePomodoro, TimerState, Cycle } from "./pomodoro";
+import type { Seconds } from "./pomodoro";
 
 function App() {
+  const pomodoro = usePomodoro();
+
+  const interval = React.useRef<number>(-1);
+  const intervalCallback = React.useRef<() => void>(intervalCb);
+
+  React.useEffect(() => {
+    intervalCallback.current = intervalCb;
+  });
+
+  React.useEffect(() => {
+    return () => resetInterval();
+  }, []);
+
+  function intervalCb() {
+    const isFinished = pomodoro.tick();
+    if (isFinished) finishTimer();
+  }
+
+  function resetInterval() {
+    if (interval.current !== -1) {
+      clearInterval(interval.current);
+      interval.current = -1;
+    }
+  }
+
+  function startTimer() {
+    pomodoro.start();
+
+    interval.current = window.setInterval(() => {
+      intervalCallback.current();
+    }, 1000);
+  }
+
+  function finishTimer() {
+    pomodoro.finish();
+    resetInterval();
+  }
+
+  function pauseTimer() {
+    pomodoro.pause();
+    resetInterval();
+  }
+
+  function nextTimer() {
+    pomodoro.next();
+  }
+
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Pomodoro</h1>
+      <h2>{printCycle(pomodoro.cycle)}</h2>
+
+      <Timer time={pomodoro.timer} />
+
+      <TimerControls
+        onStart={startTimer}
+        onPause={pauseTimer}
+        onNext={nextTimer}
+        state={pomodoro.timerState}
+      />
+
+      <p>Session: {pomodoro.sessionCount}/3</p>
     </div>
   );
+}
+
+type TimerProps = { time: Seconds }
+
+function Timer({ time }: TimerProps) {
+  return(
+    <p>{printTimer(time)}</p>
+  );
+}
+
+type TimerControlsProps = {
+  onStart: () => void;
+  onPause: () => void;
+  onNext: () => void;
+  state: TimerState; 
+}
+
+function TimerControls(props: TimerControlsProps) {
+  if (props.state === TimerState.Idle || props.state === TimerState.Paused) {
+    return(
+      <button onClick={props.onStart}>
+        Start
+      </button>
+    );
+  }
+
+  if (props.state === TimerState.Ticking) {
+    return(
+      <button onClick={props.onPause}>
+        Pause
+      </button>
+    );
+  }
+
+  return(
+    <button onClick={props.onNext}>
+      Next
+    </button>
+  );
+}
+
+function printTimer(value: Seconds): string {
+  const min = Math.floor(value / 60);
+  const sec = value % 60;
+
+  return [min, sec].map(v => String(v).padStart(2, "0")).join(":");
+}
+
+function printCycle(cycle: Cycle) {
+  const names = {
+    [Cycle.Work]: "Work",
+    [Cycle.Break]: "Break",
+    [Cycle.LongBreak]: "Long Break"
+  };
+
+  return names[cycle];
 }
 
 export default App;
--- src/pomodoro.ts
+++ src/pomodoro.ts
@@ -1,79 +1,93 @@
+import React from "react";
+
 type Seconds = number
 
-enum CycleName {
-  Work = 0,
-  Break,
-  LongBreak
-}
-
-type Cycle = {
-  name: CycleName;
-  time: Seconds;
+enum Cycle {
+  Work = "WORK",
+  Break = "BREAK",
+  LongBreak = "LONG_BREAK"
 }
 
 enum TimerState {
-  Idle = 0,
-  Ticking,
-  Paused,
-  Finished
+  Idle = "IDLE",
+  Ticking = "TICKING",
+  Paused = "PAUSED",
+  Finished = "FINISHED"
 }
 
-type Timer = {
-  value: Seconds;
-  state: TimerState;
-}
-
-class Pomodoro {
+type usePomodoroHook = {
+  tick: () => boolean;
+  start: () => void;
+  finish: () => void;
+  pause: () => void;
+  next: () => void;
   cycle: Cycle;
-  timer: Timer;
+  timer: Seconds;
+  timerState: TimerState;
   sessionCount: number;
-
-  constructor(c: Cycle, t: Timer) {
-    this.sessionCount = 0;
-    this.cycle = c;
-    this.timer = t;
-  }
-
-  tick(): boolean {
-    this.timer.value -= 1;
-    return this.timer.value === 0;
-  }
-
-  start() {
-    this.timer.state = TimerState.Ticking;
-
-    if (this.cycle.name === CycleName.Work) {
-      this.sessionCount += 1;
-    }
-  }
-
-  finish() {
-    this.timer.state = TimerState.Finished;
-  }
-
-  pause() {
-    this.timer.state = TimerState.Paused;
-  }
-
-  next() {
-    this.timer.state = TimerState.Idle;
-
-    if (this.cycle.name === CycleName.Break || this.cycle.name === CycleName.LongBreak) {
-      if (this.cycle.name === CycleName.LongBreak) {
-        this.sessionCount = 0;
-      }
-      this.setCycle({ name: CycleName.Work, time: 5 });
-    } else if (this.sessionCount >= 3) {
-      this.setCycle({ name: CycleName.LongBreak, time: 3 });
-    } else {
-      this.setCycle({ name: CycleName.Break, time: 1 });
-    }
-  }
-
-  setCycle(newCycle: Cycle) {
-    this.cycle = newCycle;
-    this.timer.value = newCycle.time;
-  }
 }
 
-export { Pomodoro, CycleName, TimerState };
+function usePomodoro(): usePomodoroHook {
+  const [cycle, setCycle] = React.useState<Cycle>(Cycle.Work);
+  const [timer, setTimer] = React.useState<Seconds>(5);
+  const [timerState, setTimerState] = React.useState<TimerState>(TimerState.Idle);
+  const [sessionCount, setSessionCount] = React.useState<number>(0);
+
+  function tick(): boolean {
+    const newTimer = timer - 1;
+    setTimer(newTimer);
+    return newTimer <= 0;
+  }
+
+  function start() {
+    if (cycle === Cycle.Work && timerState === TimerState.Idle) {
+      setSessionCount(sessionCount + 1);
+    }
+    setTimerState(TimerState.Ticking);
+  }
+
+  function finish() {
+    setTimerState(TimerState.Finished);
+  }
+
+  function pause() {
+    setTimerState(TimerState.Paused);
+  }
+
+  function next() {
+    setTimerState(TimerState.Idle);
+
+    if (cycle === Cycle.Break || cycle === Cycle.LongBreak) {
+      if (cycle === Cycle.LongBreak) {
+        setSessionCount(0);
+      }
+
+      // TODO: Add max session count and cycles time to settings
+      setNewCycle(Cycle.Work, 5);
+    } else if (sessionCount >= 3) {
+      setNewCycle(Cycle.LongBreak, 5);
+    } else {
+      setNewCycle(Cycle.Break, 1);
+    }
+  }
+
+  function setNewCycle(newCycle: Cycle, time: Seconds) {
+    setCycle(newCycle);
+    setTimer(time);
+  }
+
+  return {
+    tick,
+    start,
+    finish,
+    pause,
+    next,
+    cycle,
+    timer,
+    timerState,
+    sessionCount
+  };
+}
+
+export { usePomodoro, Cycle, TimerState };
+export type { Seconds };
