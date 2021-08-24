f2486898e07a8f73debe7f0f6f7aa95f9f680e8f
Mark Fosseddy <markfosseddy@gmail.com>
2021-08-24 12:26:30 +0500

    switch to vue

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Vue from "vue";
+import TestComponent from "./components/test.vue";
+
+export default Vue.extend({
+  name: "App",
+  components: { TestComponent }
+});
+</script>
+
+<template lang="pug">
+  div
+    h1 Hello, World
+
+    TestComponent
+</template>
